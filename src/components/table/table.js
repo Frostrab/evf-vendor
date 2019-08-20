481d421ffc5a295046cda18d0a7cefdf16ff752e
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2019-08-20 16:35:29 +0700

    create component table responsive

---
+++ src/components/table/table.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Table } from 'antd'
+import 'antd/dist/antd.css'
+
+class TableForm extends React.Component {
+  render() {
+    return (
+      <Table
+        columns={this.props.columns}
+        dataSource={this.props.dataSource}
+        bordered
+        rowClassName={() => 'responsive-row'}
+        pagination={false}
+        expandedRowRender={this.props.expandedRowRender}
+      />
+    )
+  }
+}
+export default TableForm
--- src/pages/Content1.js
+++ src/pages/Content1.js
@@ -1,4 +1,21 @@
 import React from 'react'
-import Card from '../components/Card'
-const MasterList = () => <Card title={'Page1'}>5555</Card>
+import Paper from '../components/Card'
+import List from '../components/list'
+import Table from '../components/table'
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+]
+const MasterList = () => (
+  <React.Fragment>
+    <Paper title={'Page1'}>
+      <List data={data} view edit bordered />
+      <Table />
+    </Paper>
+  </React.Fragment>
+)
 export default MasterList
